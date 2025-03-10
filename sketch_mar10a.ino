d804c4ad01df296dcf1ea6b16a95a39f586ad1c7
natwillett <ngwi230@uky.edu>
2025-03-10 14:43:28 -0400

    Revert "Update sketch_mar10a.ino"
    This reverts commit 49fd87702191c102133f1adf9ec839ce125e4154.

--- sketch_mar10a.ino
+++ sketch_mar10a.ino
@@ -7,6 +7,4 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
-const int PWMB = 10; //speed control pin on the motor driver for the left motor
-const int BIN2 = 9; //control pin 2 on the motor driver for the left motor
-const int BIN1 = 8; //control pin 1 on the motor driver for the left motor
+Hello
