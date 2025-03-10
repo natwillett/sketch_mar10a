49fd87702191c102133f1adf9ec839ce125e4154
natwillett <ngwi230@uky.edu>
2025-03-10 14:39:48 -0400

    Update sketch_mar10a.ino
    Deleted initial line, added 3 example lines

--- sketch_mar10a.ino
+++ sketch_mar10a.ino
@@ -7,4 +7,6 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
-Hello
+const int PWMB = 10; //speed control pin on the motor driver for the left motor
+const int BIN2 = 9; //control pin 2 on the motor driver for the left motor
+const int BIN1 = 8; //control pin 1 on the motor driver for the left motor
